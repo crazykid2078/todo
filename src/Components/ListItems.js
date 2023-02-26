6ad858676d2f76a251c5dedf5e5fcdf4a2a29744
sakshi2078 <sakshigatkal2003@gmail.com>
2023-02-26 19:06:03 +0530

    list

---
+++ src/Components/ListItems.js
@@ -0,0 +1,31 @@
+import "../Components/ListItems.css";
+import "../Components/ListItems.css";
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+function ListItems(props) {
+  const items = props.items;
+  const listitems = items.map((item) => {
+    return (
+      <div className="list" key={"item.key"}>
+        <p>
+          <input type="text" id={item.key} value={item.text} onChange={
+            (e) => {
+              props.setUpdate(e.target.value, item.key)
+            }
+          } />
+          <span>
+            <FontAwesomeIcon
+              className="faicons"
+              icon="trash"
+              onClick={() => props.deleteItem(item.key)}
+            />
+          </span>
+        </p>
+      </div>
+    );
+  });
+  return <div>{listitems}</div>;
+}
+
+export default ListItems;
